0ba480f6a021c4e473d3701577c87b948646362a
Tomáš Štveráček <tomas.stveracek@gmail.com>
2023-11-15 23:17:10 +0100

    import svg icons

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
+import "./icons/about.svg";
+import "./icons/add.svg";
+import "./icons/articles.svg";
+import "./icons/helpdesk.svg";
+import "./icons/login.svg";
+import "./icons/logout.svg";
