64650f2937a4b7f473c849ddc9bfde2833d13dfa
kucera41 <kucera41@student.vspj.cz>
2023-11-19 19:31:04 +0100

    Dodělávky v admin dashboardu a CRUD  článků

--- src/Controller/AutorController.php
+++ src/Controller/AutorController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PrispevekRepository;
+use App\Repository\VydaniRepository;
 
 class AutorController extends AbstractController
 {
     #[Route('/autor', name: 'app_autor')]
-    public function index(): Response
+    public function index(PrispevekRepository $prispevekRepository, VydaniRepository $vydaniRepository): Response
     {
+        $prispevky = $prispevekRepository->findAll();
+
+        $vydaniData = [];
+        foreach ($prispevky as $prispevek) {
+            $vydani = $prispevek->getVydanis(); // This should return a Collection of Vydani
+            if (!$vydani->isEmpty()) {
+                $vydaniData[$prispevek->getId()] = $vydani->first(); // Get the first Vydani, or modify as needed
+            }
+        }
+
         return $this->render('Autor/index.html.twig', [
-            'controller_name' => 'AutorController',
+            'prispevky' => $prispevky,
+            'vydaniData' => $vydaniData,
         ]);
     }
 }
